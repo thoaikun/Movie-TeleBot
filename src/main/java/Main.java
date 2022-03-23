8df904a6588d7ddfdfcc38aa29cc23587483d594
Huy-Thai-3105 <80185943+Huy-Thai-3105@users.noreply.github.com>
2022-03-23 09:26:09 +0700

    Delete src/main/java directory

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
